a5f2210ed9bfd88dfaa65c34613998a436b6ca37
JonSnow88 <spamking101@gmail.com>
2024-09-17 19:12:02 -0400

    Update pbs-install.sh

--- install/pbs-install.sh
+++ install/pbs-install.sh
@@ -5,7 +5,35 @@
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
 source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+#color
+#verb_ip6
+#catch_errors
+#setting_up_container
+#network_check
+#update_os
+
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+
+wget -q https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+cat <<EOF >>/etc/apt/sources.list
+deb http://download.proxmox.com/debian/pbs bookworm pbs-no-subscription
+EOF
+$STD apt-get update
+$STD apt-get install -y proxmox-backup-server
+
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+
 color
 verb_ip6
 catch_errors
